413b002daa3635bb6d1a3267ced3640992e6873c
Juan Corzo Barrera <juanpablocorzobarrera@gmail.com>
2023-12-18 10:24:08 -0300

    first commit

---
+++ src/components/Square/Square.jsx
@@ -0,0 +1,22 @@
+import '../Square/Square.css'
+import classNames from 'classnames';
+function Square({value,onClick,turn}) {
+    
+  const handleClick = () =>{
+    (turn !=- null && value === null) && onClick()
+    
+  }
+  
+  let squareClass = classNames({
+    square: true,
+    [`square--${value}`]: value !== null,
+  });
+
+  return (
+    <div className={squareClass} onClick={()=>handleClick()}>
+        
+    </div>
+  )
+}
+
+export default Square
